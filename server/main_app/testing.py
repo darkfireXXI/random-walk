e4e8f9da06753934ad65ccb7fef12af033c8c443
darkfirexxi <aselz21@gmail.com>
2025-01-05 20:52:10 -0500

    whip work, then rest

--- server/main_app/testing.py
+++ server/main_app/testing.py
@@ -2,12 +2,13 @@ import json
 
 import requests
 
-url = "http://0.0.0.0:50051/register"
+url = "http://0.0.0.0:50051"
+# url = "http://0.0.0.0:50051/register"
 # url = "http://127.0.0.1:50051/"
 
-# r = requests.get(url)
-payload = {"user_name": "user_name", "password": "password", "email": "email"}
-r = requests.post(url, data=json.dumps(payload))
+r = requests.get(url)
+# payload = {"user_name": "user_name", "password": "password", "email": "email"}
+# r = requests.post(url, data=json.dumps(payload))
 print(r)
 print(r.text)
 # print(r.json())
