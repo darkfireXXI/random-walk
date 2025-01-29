b86a4b5b93decd496a94f8365da0ab10a7b79685
darkfirexxi <aselz21@gmail.com>
2025-01-29 00:43:00 -0500

    idk what this even is now

--- server/main_app/db/models/relationship.py
+++ server/main_app/db/models/relationship.py
@@ -9,8 +9,8 @@ class CategorySiteRelationship(Base):
     __table_args__ = (sqlalchemy.UniqueConstraint("from_uuid", "to_uuid", "relationship", name="category_site_id_key"),)
 
     uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False, primary_key=True)
-    from_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False)
-    to_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False)
+    from_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False, index=True)
+    to_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False, index=True)
     relationship = mapped_column(sqlalchemy.Text(), nullable=False)
     status = mapped_column(sqlalchemy.Text(), nullable=True)
     submitted_by = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=True)
@@ -34,8 +34,8 @@ class UserCategoryRelationship(Base):
     __table_args__ = (sqlalchemy.UniqueConstraint("from_uuid", "to_uuid", "relationship", name="user_category_id_key"),)
 
     uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False, primary_key=True)
-    from_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False)
-    to_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False)
+    from_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False, index=True)
+    to_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False, index=True)
     relationship = mapped_column(sqlalchemy.Text(), nullable=False)
     created_at = mapped_column(TIMESTAMP, nullable=False)
     updated_at = mapped_column(TIMESTAMP, nullable=False)
@@ -55,8 +55,8 @@ class UserSiteRelationship(Base):
     __table_args__ = (sqlalchemy.UniqueConstraint("from_uuid", "to_uuid", "relationship", name="user_site_id_key"),)
 
     uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False, primary_key=True)
-    from_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False)
-    to_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False)
+    from_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False, index=True)
+    to_uuid = mapped_column(sqlalchemy.UUID(as_uuid=True), nullable=False, index=True)
     relationship = mapped_column(sqlalchemy.Text(), nullable=False)
     created_at = mapped_column(TIMESTAMP, nullable=False)
     updated_at = mapped_column(TIMESTAMP, nullable=False)
